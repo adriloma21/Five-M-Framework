36e683764618541d2cff456ca6dfbc0b16c861b6
Adrian Lopez <adrian.lopez@ikasle.egibide.org>
2024-03-02 23:51:06 +0100

    Five M Server Start

---
+++ core/sv_core.lua
@@ -0,0 +1,28 @@
+RegisterNetEvent('MP-Base:ServerStart')
+AddEventHandler('MP-Base:ServerStart', function()
+    print('Server Started')
+    local civ = source
+    Citizen.CreateThread(function()
+        local Identifier = GetPlayerIdentifiers(civ)[1] -- This gets Steam:192913831
+        if not Identifier then
+            DropPlayer(civ, 'Identifier not found')
+        end
+        return
+    end)
+end)
+
+RegisterNetEvent('MP-Base:client:getObject')
+AddEventHandler('MP-Base:client:getObject', function(callback)
+    callback(MP)
+end)
+
+-- Commands
+AddEventHandler('MP-Base:addCommand', function(command, callback, suggestion, args)
+    MP.Functions.addCommand(command, callback, suggestion, args)
+end)
+
+AddEventHandler('MP-Base:addGroupCommand', function(group, command, callback, callbackfailed, suggestion, args)
+    MP.Functions.addGroupCommand(group, command, callback, callbackfailed, suggestion, args)
+end)
+
+-- Callback Server
