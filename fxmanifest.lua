c37d36eb5d3cd7b3c57f5310beab7bfc127fdef6
Adrian Lopez <adrian.lopez@ikasle.egibide.org>
2024-03-03 00:03:20 +0100

    fxmanifest created

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+
+--dependency 'ghmattimysql'
+
+client_scripts {
+    'core/cl_core.lua',
+    --'client.lua',
+    --'config.lua'
+}
+
+server_scripts {
+    'core/cl_core.lua',
+    --'server.lua',
+    --'config.lua'
+}
