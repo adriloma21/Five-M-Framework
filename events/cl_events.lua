ba6ef6725682087827316a2a5c73648a96d3b93e
Adrian Lopez <adrian.lopez@ikasle.egibide.org>
2024-03-03 00:17:51 +0100

    Comentarios Events

---
+++ events/cl_events.lua
@@ -0,0 +1,22 @@
+-- Call MP
+
+MP.Functions = MP.Functions or {}
+MP.RequestID = MP.RequestID or {}
+MP.ServerCallback = MP.ServerCallback or {}
+MP.ServerCallbacks = MP.ServerCallbacks or {}
+MP.CurrentRequestId = MP.CurrentRequestId or {}
+
+MP.Functions.GetKey = function(key)
+    local Keys = {
+        ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+        ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["0"] = 160,
+        ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303,
+        ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18, ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74,
+        ["K"] = 311, ["L"] = 182, ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82,
+        ["."] = 81, ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+        ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173, ["NENTER"] = 201, ["N4"] = 96, ["N5"] = 97, ["N6"] = 98, ["N+"] = 99, ["N-"] = 100,
+        ["N7"] = 101, ["N8"] = 102, ["N9"] = 103
+    }
+    return Keys[key]
+end
+
