77bcc441b7cc69a3ef4a7bf15b753c66c41c16a4
Adrian Lopez <adrian.lopez@ikasle.egibide.org>
2024-03-03 17:52:11 +0100

    new character cash & bank

---
+++ resources/shared/shared.lua
@@ -0,0 +1,16 @@
+MP = MP or {}
+MP.Base = MP.Base or {}
+MP.Player = MP.Player or {}
+MP.Players = MP.Playerss or {}
+MP.APlayer = MP.APlayer or {}
+MP.APlayers = MP.APlayers or {}
+MP.Functions = MP.Functions or {}
+MP.PlayerData = MP.PlayerData or {}
+MP.Commands = MP.Commands or {}
+MP.Admin = MP.Admin or {}
+
+MP.NewCharacter = {
+    Cash = 2500,
+    Bank = 80000,
+}
+
